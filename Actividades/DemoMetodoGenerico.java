f17abb33f78599f7a6c9d9abf10649f679f6941c
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-29 12:54:07 -0500

    Update DemoMetodoGenerico.java

--- Actividades/DemoMetodoGenerico.java
+++ Actividades/DemoMetodoGenerico.java
@@ -11,25 +11,22 @@ class DemoMetodoGenerico {
                 return false; //arrays diferentes
         return true; // Contenido de arrays son equivalentes
     }
-    public static void main(String[] args) {
-    Integer nums[]={1,2,3,4,5};
-    Integer nums2[]={1,2,3,4,5};
-    Integer nums3[]={1,2,7,4,5};
-    Integer nums4[]={1,2,7,4,5,6};
-    if (igualArrays(nums,nums))
-    System.out.println("nums es igual a nums");
-    if (igualArrays(nums,nums2))
-    System.out.println("nums es igual a nums2");
-    if (igualArrays(nums,nums3))
-    System.out.println("nums es igual a num3");
-    if (igualArrays(nums,nums4))
-    System.out.println("nums es igual a nums4");
-    // Crea un array de double
-    //Double dvals[]={1.1,2.2,3.3,4.4,5.5};
-    //if(igualArrays(nums,dvals)) // System.out.println("nums es igual a dvals");
-    //A
-    //B
-    //C
-    //D
+        public static void main(String[] args) {
+        Integer nums[]={1,2,3,4,5};
+        Integer nums2[]={1,2,3,4,5};
+        Integer nums3[]={1,2,7,4,5};
+        Integer nums4[]={1,2,7,4,5,6};
+        if (igualArrays(nums,nums))
+            System.out.println("nums es igual a nums");
+        if (igualArrays(nums,nums2))
+            System.out.println("nums es igual a nums2");
+        if (igualArrays(nums,nums3))
+            System.out.println("nums es igual a num3");
+        if (igualArrays(nums,nums4))
+            System.out.println("nums es igual a nums4");
+
+        Double dvals[]={1.1,2.2,3.3,4.4,5.5};
+    if(igualArrays(nums,dvals)) 
+    System.out.println("nums es igual a dvals");
     }
 }
