354565218a98b6d5ebf0f5b59308f409957f7b7d
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-29 10:46:11 -0500

    Update Principal.java

--- Actividades/Principal.java
+++ Actividades/Principal.java
@@ -2,12 +2,11 @@ package Actividades;
 
 public class Principal {
     public static void main(String[] args){
-        Bolsa < Chocolatina > BolsaCho = new Bolsa < Chocolatina > ();
+        Bolsa < Chocolatina > BolsaCho = new Bolsa < Chocolatina > (3);
         Chocolatina c = new Chocolatina("Milky");
         Chocolatina c1 = new Chocolatina("Milky");
         Chocolatina c2 = new Chocolatina("Ferrero");
         BolsaCho.add(c);
         BolsaCho.add(c1);
-        
     }
 }
