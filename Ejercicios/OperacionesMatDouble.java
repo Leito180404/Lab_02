8271e2e23c0086ef231090b4a2b47b02d9d19d48
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-29 13:35:47 -0500

    Update OperacionesMatDouble.java

--- Ejercicios/OperacionesMatDouble.java
+++ Ejercicios/OperacionesMatDouble.java
@@ -1,5 +1,14 @@
 package Ejercicios;
 
-public class OperacionesMatDouble {
-    
+public class OperacionesMatDouble implements Operable<Double>{
+    private Double valor;
+
+    public OperacionesMatDouble(Double valor){
+        this.valor = valor;
+    }
+
+    @Override
+    public Double suma(Double otro){
+        return valor + otro;
+    }
 }
