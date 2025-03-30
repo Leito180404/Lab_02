65a1d7333158684fe846f84e29ba7e3d7dd8a2a3
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-03-29 20:26:19 -0500

    .

--- Ejercicios/Principal.java
+++ Ejercicios/Principal.java
@@ -1,5 +1,91 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Principal {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Seleccione el tipo de operacion:");
+        System.out.println("1. Operaciones con Enteros");
+        System.out.println("2. Operaciones con Decimales");
+        int opcionTipo = sc.nextInt();
+
+        switch (opcionTipo) {
+
+            case 1:
+                System.out.println("Ingrese el primer numero entero:");
+                int numEntero1 = sc.nextInt();
+
+                System.out.println("Ingrese el segundo numero entero:");
+                int numEntero2 = sc.nextInt();
+
+                OperacionesMatInteger opEntero = new OperacionesMatInteger(numEntero1);
+
+                System.out.println("Ingrese la operacion (1:suma, 2:resta, 3:producto, 4:division):");
+                int operacionEntero = sc.nextInt();
+
+                switch (operacionEntero) {
+                    case 1:
+                        System.out.println("Suma: " + opEntero.suma(numEntero2));
+                        break;
+                    case 2:
+                        System.out.println("Resta: " + opEntero.resta(numEntero2));
+                        break;
+                    case 3:
+                        System.out.println("Producto: " + opEntero.producto(numEntero2));
+                        break;
+                    case 4:
+                        try {
+                            System.out.println("Division: " + opEntero.division(numEntero2));
+                        } catch (ArithmeticException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                    default:
+                        System.out.println("Operacion invalida.");
+                }
+                break;
+
+            case 2:
+                System.out.println("Ingrese el primer numero decimal:");
+                double numDouble1 = sc.nextDouble();
+
+                System.out.println("Ingrese el segundo numero decimal:");
+                double numDouble2 = sc.nextDouble();
+
+                OperacionesMatDouble opDouble = new OperacionesMatDouble(numDouble1);
+
+                System.out.println("Ingrese la operacion (1:suma, 2:resta, 3:producto, 4:division):");
+                int operacionDouble = sc.nextInt();
+
+                switch (operacionDouble) {
+                    case 1:
+                        System.out.println("Suma: " + opDouble.suma(numDouble2));
+                        break;
+                    case 2:
+                        System.out.println("Resta: " + opDouble.resta(numDouble2));
+                        break;
+                    case 3:
+                        System.out.println("Producto: " + opDouble.producto(numDouble2));
+                        break;
+                    case 4:
+                        if (numDouble2 != 0) {
+                            System.out.println("Division: " + opDouble.division(numDouble2));
+                        } else {
+                            System.out.println("No se puede dividir por cero.");
+                        }
+                        break;
+                    default:
+                        System.out.println("Operacion invalida.");
+                }
+                break;
+
+            default:
+                System.out.println("Tipo de operacion invalido.");
+        }
+
+        sc.close();
+    }
 }
---
+++ Ejercicios/Prueba.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class Prueba {
+    public static void main(String[] args) {
+        
+        OperacionesMatInteger operacionesMatInteger = new OperacionesMatInteger(10);
+        System.out.println("Suma: " + operacionesMatInteger.suma(5));
+
+        OperacionesMatDouble operacionesMatDouble = new OperacionesMatDouble(10.5);
+        System.out.println("Suma: " + operacionesMatDouble.suma(4.5));
+    }
+}
+
+
